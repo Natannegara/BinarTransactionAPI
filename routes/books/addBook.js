36a6b5566d1d19859869b4304d3726dab8c1ac4a
sudihardi <sudyhardy@gmail.com>
2020-10-26 21:41:33 +0700

    updated

--- routes/books/addBook.js
+++ routes/books/addBook.js
@@ -4,7 +4,7 @@ const addData = require('../../controllers/addController');
 const app = express.Router()
 
 
-app.post("/books", verifyJwt('seller'), (req, res) => {
+app.post("/books", verifyJwt("seller"), (req, res) => {
     const body = req.body;
     const result = addData('books', body);
     res.send(result);
